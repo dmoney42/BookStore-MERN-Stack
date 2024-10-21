f47e7bcef28c86885bd63f4d395a323e1bc5f3f9
Demetrius Sullivan <dssullivan1@gmail.com>
2024-10-21 13:59:01 -0400

    we are creating the backend of our program using expressjs, nodejs. we connected our program to mongodb online and allowed our program to create, add and update a book from our database.

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://dssullivan1:NCWssmAMwuZ9IBe7@book-store-mern.zb3v1.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store-MERN';
