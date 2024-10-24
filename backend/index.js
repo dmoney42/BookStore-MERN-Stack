32de60dd2b2c5f745beee10e6064dfdbcda9019b
Demetrius Sullivan <dssullivan1@gmail.com>
2024-10-23 21:47:03 -0400

    we are installing cors package into our project to handle cross origin issues, we created our front end using vite and javascript, we created our frontend pages for createbook, deletebook, editbook, home and showbook. we installed tailwind css, route packages

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,21 @@ import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import booksRoute from './routes/booksRoute.js';
 
+
 const app = express();
  
 app.use(express.json());
+
+//Middleware for handling CORS POLICY
+//Option 1: Allow All Origins with Default of cors(*)
+app.use(cors())
+
+//Option 2: allow custom origins
+/*app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}));*/
 
 app.get('/',(request,response)=>{
     console.log(request)
