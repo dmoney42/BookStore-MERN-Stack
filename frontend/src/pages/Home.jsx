6829f38976add4ec786df3fa03dbdcb3fae5efe2
Demetrius Sullivan <dssullivan1@gmail.com>
2024-10-29 22:17:00 -0400

    Oct 29 2024 commit before pulling changes from BookStore-Bug-fix branch

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -60,7 +60,41 @@ const Home = () => {
           <tbody>
             {books.map((book) => (
               <tr key={book._id} className="h-8">
-                
+                      <td className='border border-slate-700 rounded-md text-center'>
+                        {index + 1}
+                      </td>    
+
+                      <td className='border border-slate-700 rounded-md text-center'>
+                        {book.title}
+                      </td>
+
+                      <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+                        {book.author}
+                      </td>
+
+                      <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+                        {book.publishYear}
+                      </td>
+              
+
+                      <td className='border border-slate-700 rounded-md text-center'>
+                        <div className='flex justify-center gap-x-4'>
+                            <Link to={`/books/details/${book._id}`}>
+                              <BsInfoCircle className='text-2x1 text-green-800'/>
+                            </Link>
+
+                            <Link to={`/books/edit/${book._id}`}>
+                              <AiOutLineEdit className='text-2x1 text-yellow-600'/>
+                            </Link>
+
+                            <Link to={`/books/edit/${book._id}`}>
+                              <MdOutlineDelete className='text-2x1 text-'/>
+                            </Link>
+
+                        </div>
+                      </td>
+
+
               </tr>
             ))}
           </tbody>
