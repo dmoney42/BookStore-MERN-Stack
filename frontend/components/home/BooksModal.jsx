f56a5953f1aa4629c13d788a8efea0ce9503bf80
Demetrius Sullivan <dssullivan1@gmail.com>
2024-11-17 14:21:52 -0500

    Nov 17 2024 - we fixed the issue with BooksCard.jsx not displaying the table correctly when you click the Card button. we fixed the table so that i will display the full width of the page. we created BooksModal.jsx

---
+++ frontend/components/home/BooksModal.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { AiOutlineClose } from 'react-icons/ai'
+import { PiBookOpenTextLight } from 'react-icons/pi'
+import { BiUserCircle } from 'react-icons/bi'
+
+
+const BooksModal = ({ book, onClose}) => {
+  return (
+    <div className='fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center'
+    
+    >
+        BooksModal
+    </div>
+  )
+}
+
+export default BooksModal
